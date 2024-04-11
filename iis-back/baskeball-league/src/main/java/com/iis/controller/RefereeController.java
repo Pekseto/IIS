ef6f07ca4a6779cbceb9c27cb7df11484873a109
Luka Todorovic <86576899+ToD0R@users.noreply.github.com>
2024-04-11 22:34:35 +0200

    fix: Login from front fix

--- iis-back/baskeball-league/src/main/java/com/iis/controller/RefereeController.java
+++ iis-back/baskeball-league/src/main/java/com/iis/controller/RefereeController.java
@@ -6,30 +6,33 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/judge")
+@CrossOrigin
+@RequestMapping("/api/referee")
 @Tag(name = "Referee")
 public class RefereeController {
 
     private final RefereeService refereeService;
 
     @PostMapping("/register")
-    @Operation(summary = "Register new judge")
+    @Operation(summary = "Register new referee")
+    @PreAuthorize("hasRole('ROLE_LEAGUE_ADMIN')")
     public ResponseEntity<RefereeDTO> regiter(@RequestBody RefereeDTO referee) {
         return ResponseEntity.ok(refereeService.Register(referee));
     }
 
-    @GetMapping("/getJudge/{id}")
-    @Operation(summary = "Get judge data by id")
+    @GetMapping("/getReferee/{id}")
+    @Operation(summary = "Get referee data by id")
     public ResponseEntity<RefereeDTO> getJudge(@PathVariable("id") Long id) {
         return ResponseEntity.ok(refereeService.GetById(id));
     }
 
     @PutMapping("/update")
-    @Operation(summary = "Update data about judge")
+    @Operation(summary = "Update data about referee")
     public ResponseEntity<RefereeDTO> update(@RequestBody RefereeDTO judge) {
         return ResponseEntity.ok(refereeService.Update(judge));
     }
--- iis-front/src/app/login/login.component.ts
+++ iis-front/src/app/login/login.component.ts
@@ -29,7 +29,7 @@ export class LoginComponent {
     if (this.loginForm.valid) {
       this.authService.login(login).subscribe({
         next: () => {
-          this.router.navigate(['/home']);
+          this.router.navigate(['/']);
         },
       });
     }
